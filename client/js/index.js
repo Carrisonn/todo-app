bbf48557532d7f591054203946b5b80a9a9698d9
Carrison <carlos97181034535@gmail.com>
2025-10-07 21:43:11 +0200

    Add form submission handling and task validation in index.js

--- client/js/index.js
+++ client/js/index.js
@@ -1,9 +1,25 @@
 import Tasks from './classes/Tasks.js'
 import UI from './classes/UI.js'
+import ClientValidations from './classes/ClientValidations.js'
+import { $form } from './DOMelements.js'
 
 async function InitApp() {
   const tasks = await Tasks.getTasks()
   UI.renderTasks(tasks)
+  $form.addEventListener('submit', handleSubmit)
+}
+
+async function handleSubmit(event) {
+  event.preventDefault()
+  const formData = new FormData($form)
+  const task = formData.get('task').trim()
+  const priority = formData.get('priority')
+  const isValidTask = ClientValidations.validateTasks({ task, priority })
+  if (!isValidTask) return UI.feedbackMessage('Por favor, completa todos los campos', 'error')
+  //const formattedTask = task.replace(/^\w/, character => character.toUpperCase());
+  //const tasks = await Tasks.addTask({ task: formattedTask, priority })
+  //UI.renderTasks(tasks)
+  $form.reset()
 }
 
 InitApp()
