44ac6be86d99d9f00a6ce1036bd0d68f611dde4f
Carrison <carlos97181034535@gmail.com>
2025-10-07 21:42:30 +0200

    Update the Task API route to remove the 'api' prefix

--- routes/router.js
+++ routes/router.js
@@ -3,7 +3,7 @@ import Controller from '../controller/Controller.js'
 
 const router = express.Router()
 
-router.get('/api/tasks', Controller.getTasks)
+router.get('/tasks', Controller.getTasks)
 
 
 export default router
