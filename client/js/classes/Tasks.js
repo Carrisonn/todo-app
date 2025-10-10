1fd2a54ef19511c61e8a7a54379abba596b3360d
Carrison <carlos97181034535@gmail.com>
2025-10-10 21:33:39 +0200

    Implement addTask method to add tasks with priority and error handling

--- client/js/classes/Tasks.js
+++ client/js/classes/Tasks.js
@@ -11,7 +11,20 @@ export default class Tasks {
     }
   }
 
-  //static async addTask(task) {
-  //
-  //}
+  static async addTask({ task, priority }) {
+    const url = '/tasks'
+    const POST_CONFIG = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ task, priority })
+    }
+    try {
+      const response = await fetch(url, POST_CONFIG)
+      const data = await response.json()
+      if (!response.ok) return data // mensaje de error del servidor
+      return data
+    } catch (error) {
+      console.error(error)
+    }
+  }
 }
